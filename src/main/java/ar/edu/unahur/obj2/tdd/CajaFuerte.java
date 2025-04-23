f8cc140005c9164f05c82b01afc309770059e63b
FrancoColombano <francocolombano@gmail.com>
2025-04-23 19:53:28 -0300

    Terminado, 100% coverage

--- src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
+++ src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
@@ -1,5 +1,24 @@
 package ar.edu.unahur.obj2.tdd;
 
 public class CajaFuerte {
+    private Boolean estaAbierta = true;
+    private Integer password;
+
+    public Boolean estaAbierta() {
+        return estaAbierta;
+    }
+
+    public void cerrar(Integer password) {
+        if (this.estaAbierta) {
+            this.password = password;
+            this.estaAbierta = false;
+        }
+    }
+
+    public void abrir(Integer password) {
+        if (password.equals(this.password)) {
+            this.estaAbierta = true;
+        }
+    }
 
 }
--- src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
+++ src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
@@ -6,5 +6,44 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class CajaFuerteTest {
+    @Test
+    public void cajaFuerteDebeEstarAbiertaAlCrearse(){
+        CajaFuerte caja = new CajaFuerte();
+        assertTrue(caja.estaAbierta());
+    }
 
+    @Test
+    public void alIngresarCodigoLaCajaSeCierra(){
+        CajaFuerte caja = new CajaFuerte();
+        caja.cerrar(1234);
+        assertFalse(caja.estaAbierta());
+    }
+
+    
+    @Test
+    public void laCajaPuedeEstablecerNuevoCodigoSoloSiEstaAbierta(){
+        CajaFuerte caja = new CajaFuerte();
+        caja.cerrar(1234);
+        assertFalse(caja.estaAbierta());
+        caja.cerrar(4567);
+        caja.abrir(4567);
+        assertFalse(caja.estaAbierta());
+    }
+
+    @Test
+    public void alIngresarCodigoCorrectoLaCajaSeAbre(){
+        CajaFuerte caja = new CajaFuerte();
+        caja.cerrar(1234);
+        assertFalse(caja.estaAbierta());
+        caja.abrir(1234);
+        assertTrue(caja.estaAbierta());
+    }
+
+    @Test void alIngresarCodigoIncorrectoLaCajaNoSeAbre(){
+        CajaFuerte caja = new CajaFuerte();
+        caja.cerrar(1234);
+        assertFalse(caja.estaAbierta());
+        caja.abrir(4567);
+        assertFalse(caja.estaAbierta());
+    }
 }
